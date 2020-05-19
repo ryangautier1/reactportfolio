68d0218d8cc626fc6a651837a94c195cd2ffa88d
Ryan Gautier <ryangautier2@gmail.com>
2020-05-19 18:24:39 -0500

    add project background to project page on page load

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import HomeContent from '../components/HomeContent';
+import "../components/Background/style.css";
 
 function Projects() {
+  const loadbackground = () => {
+    document.body.removeAttribute("class");
+    document.body.classList.add("project-background");
+  }
+
+  useEffect(() => {
+    loadbackground();
+  }, [])
+
   return (
     <div>
       <HomeContent />
