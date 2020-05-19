362e5cbf19fd10bfd810ea53a69327deffb3bb68
Ryan Gautier <ryangautier2@gmail.com>
2020-05-18 23:06:18 -0500

    style navbar, change background image

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,11 +3,11 @@ import './style.css';
 
 function Navbar() {
   return (
-    <nav className="w-full py-4 relative right-0 z-10">
-      <div className="inline-block mx-3 text-2xl nav-item">Home</div>
-      <div className="inline-block mx-3 text-2xl nav-item">About</div>
-      <div className="inline-block mx-3 text-2xl nav-item">Projects</div>
-      <div className="inline-block mx-3 text-2xl nav-item">Contact</div>
+    <nav className="flex flex-col z-10 relative items-center justify-between text-gray-100">
+      <div className="mx-3 block text-2xl nav-item">Home</div>
+      <div className="mx-3 block text-2xl nav-item">About</div>
+      <div className="mx-3 block text-2xl nav-item">Projects</div>
+      <div className="mx-3 block text-2xl nav-item">Contact</div>
     </nav>
   );
 }
