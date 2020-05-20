8424408ce40859816a11722239bf989949f8f43a
Ryan Gautier <ryangautier2@gmail.com>
2020-05-20 00:17:24 -0500

    responsive styling on about page

--- src/components/AboutContent/index.js
+++ src/components/AboutContent/index.js
@@ -4,16 +4,16 @@ import pic from "../../images/ryanpicturesquare.png"
 
 function AboutContent() {
   return (
-    <div className="relative flex flex-col about-content items-center justify-center about-page">
+    <div className="relative flex flex-col about-content items-center sm:justify-center sm:mt-0 mt-12 about-page">
       <div>
         <img src={pic} alt="Ryan Gautier" className="rounded-full picture-me mb-5" />
       </div>
-      <div className="text-gray-100 text-lg text-center about-text">
+      <div className="text-gray-100 text-md sm:text-lg text-center about-text">
         I am a web developer based in Austin, TX.
         My passion is problem solving, which is why I love what I do.
         <br /><br />
         My skillset includes
-        <span className="skills text-xl"> HTML, CSS, CSS Frameworks, JavaScript, jQuery, Node, Express, MySQL, 
+        <span className="skills text-lg sm:text-xl"> HTML, CSS, CSS Frameworks, JavaScript, jQuery, Node, Express, MySQL, 
         MongoDB, and React.</span>
         <br /><br />
         I enjoy using my creativity to create beautiful, simple solutions to complex problems. I 
