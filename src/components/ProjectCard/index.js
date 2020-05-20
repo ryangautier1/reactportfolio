866e69a2bb2c6edf243b300772e87e4f310b70e8
Ryan Gautier <ryangautier2@gmail.com>
2020-05-19 21:48:29 -0500

    add content to project card

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,30 +1,24 @@
 import React from "react";
 import "./style.css";
-import dog from "../../images/dog.png"
 
-function ProjectCard() {
+function ProjectCard(props) {
   return (
-    <div className="relative flex flex-col about-content items-center justify-center project-page">
       <div className="project-card">
-        <a href="https://ryangautier1.github.io/Lunch-and-Wag/" target="_blank" rel="noopener noreferrer">
+        <a href={props.deployed} target="_blank" rel="noopener noreferrer">
           <div className="w-full px-3 pb-3 bg-gray-300 rounded-lg">
-            <img src={dog} alt="Lunch and Wag Logo" />
-            {/* <a href="https://github.com/ryangautier1/Lunch-and-Wag" target="_blank"><img
-          src="assets/github-image.png" class="card-github" alt="Github Logo" /></a> */}
+            <img src={props.image} alt={props.alt} />
             <div>
-              <h5 class="text-xl pb-2">Lunch and Wag</h5>
-              <p class="text-md">An application for finding nearby dog-friendly restaurants, and relevant
-            information about them.</p>
+              <h5 className="text-xl pb-2">{props.title}</h5>
+              <p className="text-md">{props.description}</p>
             </div>
           </div>
         </a>
-        <a href="https://github.com/ryangautier1/Lunch-and-Wag" target="_blank" rel="noopener noreferrer">
+        <a href={props.repo} target="_blank" rel="noopener noreferrer">
           <div className="text-center bg-gray-200 rounded-lg mt-2 py-1">
             Github Repo
         </div>
         </a>
       </div>
-    </div >
   )
 }
 
--- src/components/ProjectContent/index.js
+++ src/components/ProjectContent/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./style.css";
+import dog from "../../images/dog.png"
+import ProjectCard from "../ProjectCard";
+
+const projects = [{
+  key: 1,
+  title: "Lunch and Wag",
+  image: dog,
+  alt: "Lunch and Wag Logo",
+  description: "An application for finding nearby dog-friendly restaurants, and relevant information about them.",
+  deployed: "https://ryangautier1.github.io/Lunch-and-Wag/",
+  repo: "https://github.com/ryangautier1/Lunch-and-Wag",
+}]
 
 function ProjectContent() {
   return (
     <div className="relative flex flex-col about-content items-center justify-center project-page">
-      
+      {projects.map(item => {
+        return (
+        <ProjectCard {...item} />
+        )
+      })}
       
     </div>
   )
