3a8a318a84a15416a8c4bd99c73ab160c9223fc9
Ryan Gautier <ryangautier2@gmail.com>
2020-05-20 00:01:32 -0500

    responsive styling on navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,16 +5,16 @@ import './style.css';
 function Navbar() {
   return (
     <div>
-      <div className="cursor-pointer z-20 logo text-gray-100 border-4 absolute leading-none top-0 left-0 mt-8 px-3 pb-2 pt-1 border-gray-100">
+      <div className="hidden sm:block cursor-pointer z-20 logo text-gray-100 border-4 absolute leading-none top-0 left-0 mt-8 px-3 pb-2 pt-1 border-gray-100">
         <Link to="/">
           R
         </Link>
       </div>
-      <nav className="flex flex-col z-10 absolute items-center justify-between text-gray-100">
-        <div className="mx-3 cursor-pointer block text-xl nav-item"><Link to="/">Home</Link></div>
-        <div className="mx-3 cursor-pointer block text-xl nav-item"><Link to="/about">About</Link></div>
-        <div className="mx-3 cursor-pointer block text-xl nav-item"><Link to="/projects">Projects</Link></div>
-        <div className="mx-3 cursor-pointer block text-xl nav-item"><Link to="/contact">Contact</Link></div>
+      <nav className="flex flex-row sm:flex-col z-10 relative sm:absolute sm:items-center justify-between text-gray-100">
+        <div className="mx-0 sm:mx-3 cursor-pointer block text-md sm:text-xl nav-item"><Link to="/">Home</Link></div>
+        <div className="mx-0 sm:mx-3 cursor-pointer block text-md sm:text-xl nav-item"><Link to="/about">About</Link></div>
+        <div className="mx-0 sm:mx-3 cursor-pointer block text-md sm:text-xl nav-item"><Link to="/projects">Projects</Link></div>
+        <div className="mx-0 sm:mx-3 cursor-pointer block text-md sm:text-xl nav-item"><Link to="/contact">Contact</Link></div>
       </nav>
     </div>
   );
