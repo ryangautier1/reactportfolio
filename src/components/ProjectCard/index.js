c24b1f6cb7bfacafec17aa0bdd47e4ce11132875
Ryan Gautier <ryangautier2@gmail.com>
2020-05-19 22:48:34 -0500

    style project slider

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -3,12 +3,12 @@ import "./style.css";
 
 function ProjectCard(props) {
   return (
-      <div className="project-card mx-8">
+      <div className="project-card flex-none mx-12 mb-4">
         <a href={props.deployed} target="_blank" rel="noopener noreferrer">
-          <div className="w-full px-3 pb-3 bg-gray-300 rounded-lg">
+          <div className="w-full p-3 bg-gray-300 rounded-lg">
             <img src={props.image} alt={props.alt} />
             <div>
-              <h5 className="text-xl pb-2">{props.title}</h5>
+              <h5 className="text-xl py-2">{props.title}</h5>
               <p className="text-md">{props.description}</p>
             </div>
           </div>
--- src/components/ProjectContent/index.js
+++ src/components/ProjectContent/index.js
@@ -6,34 +6,53 @@ import ProjectCard from "../ProjectCard";
 
 const projects = [
   {
-  key: 1,
-  title: "Lunch and Wag",
-  image: dog,
-  alt: "Lunch and Wag Logo",
-  description: "An application for finding nearby dog-friendly restaurants, and relevant information about them.",
-  deployed: "https://ryangautier1.github.io/Lunch-and-Wag/",
-  repo: "https://github.com/ryangautier1/Lunch-and-Wag",
+    key: 1,
+    title: "Lunch and Wag",
+    image: dog,
+    alt: "Lunch and Wag Logo",
+    description: "An application for finding nearby dog-friendly restaurants, and relevant information about them.",
+    deployed: "https://ryangautier1.github.io/Lunch-and-Wag/",
+    repo: "https://github.com/ryangautier1/Lunch-and-Wag",
   },
   {
-  key: 2,
-  title: "W4",
-  image: w4,
-  alt: "W4 Logo",
-  description: "W4 (Watch What Where When) is a movie and tv streaming locator and watchlist storing service.",
-  deployed: "https://wfourbkf.herokuapp.com/",
-  repo: "https://github.com/ryangautier1/Tv-Tracker",
+    key: 2,
+    title: "W4",
+    image: w4,
+    alt: "W4 Logo",
+    description: "W4 (Watch What Where When) is a movie and tv streaming locator and watchlist storing service.",
+    deployed: "https://wfourbkf.herokuapp.com/",
+    repo: "https://github.com/ryangautier1/Tv-Tracker",
+  },
+  {
+    key: 3,
+    title: "W4",
+    image: w4,
+    alt: "W4 Logo",
+    description: "W4 (Watch What Where When) is a movie and tv streaming locator and watchlist storing service.",
+    deployed: "https://wfourbkf.herokuapp.com/",
+    repo: "https://github.com/ryangautier1/Tv-Tracker",
+  },
+  {
+    key: 4,
+    title: "W4",
+    image: w4,
+    alt: "W4 Logo",
+    description: "W4 (Watch What Where When) is a movie and tv streaming locator and watchlist storing service.",
+    deployed: "https://wfourbkf.herokuapp.com/",
+    repo: "https://github.com/ryangautier1/Tv-Tracker",
   }
 ]
 
 function ProjectContent() {
   return (
-    <div className="relative flex flex-wrap about-content items-center justify-center project-page">
-      {projects.map(item => {
-        return (
-        <ProjectCard {...item} />
-        )
-      })}
-      
+    <div className="flex project-page items-center">
+      <div className="relative flex flex-row overflow-x-auto project-slider">
+        {projects.map(item => {
+          return (
+            <ProjectCard {...item} />
+          )
+        })}
+    </div>
     </div>
   )
 }
