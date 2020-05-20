cc9e4998fbd953a227a6180552704f1104d55865
Ryan Gautier <ryangautier2@gmail.com>
2020-05-19 23:02:22 -0500

    add readme project, style sidescroll

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -5,7 +5,7 @@ function ProjectCard(props) {
   return (
       <div className="project-card flex-none mx-12 mb-4">
         <a href={props.deployed} target="_blank" rel="noopener noreferrer">
-          <div className="w-full p-3 bg-gray-300 rounded-lg">
+          <div className="w-full p-3 bg-gray-300 rounded-lg card-block">
             <img src={props.image} alt={props.alt} />
             <div>
               <h5 className="text-xl py-2">{props.title}</h5>
--- src/components/ProjectContent/index.js
+++ src/components/ProjectContent/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.css";
 import dog from "../../images/dog.png";
 import w4 from "../../images/w4logo.png";
+import orca from "../../images/orca.png";
 import ProjectCard from "../ProjectCard";
 
 const projects = [
@@ -25,28 +26,29 @@ const projects = [
   },
   {
     key: 3,
-    title: "W4",
-    image: w4,
-    alt: "W4 Logo",
-    description: "W4 (Watch What Where When) is a movie and tv streaming locator and watchlist storing service.",
-    deployed: "https://wfourbkf.herokuapp.com/",
-    repo: "https://github.com/ryangautier1/Tv-Tracker",
+    title: "Avoid the Orcas",
+    image: orca,
+    alt: "Avoid the Orcas Logo",
+    description: "A recreation of classic Minesweeper using JavaScript.",
+    deployed: "https://ryangautier1.github.io/AvoidTheOrcas/",
+    repo: "https://github.com/ryangautier1/AvoidTheOrcas",
   },
   {
     key: 4,
-    title: "W4",
-    image: w4,
-    alt: "W4 Logo",
-    description: "W4 (Watch What Where When) is a movie and tv streaming locator and watchlist storing service.",
-    deployed: "https://wfourbkf.herokuapp.com/",
-    repo: "https://github.com/ryangautier1/Tv-Tracker",
-  }
+    title: "Read Me Generator",
+    image: "https://github.com/ryangautier1/Readme-Generator/raw/master/ImplementationGif.gif",
+    alt: "Read Me Gnereator GIF",
+    description: "A CLI application for generating README's",
+    deployed: "https://github.com/ryangautier1/Readme-Generator",
+    repo: "https://github.com/ryangautier1/Readme-Generator",
+  },
+
 ]
 
 function ProjectContent() {
   return (
     <div className="flex project-page items-center">
-      <div className="relative flex flex-row overflow-x-auto project-slider">
+      <div className="relative flex flex-row overflow-x-auto project-slider mx-4">
         {projects.map(item => {
           return (
             <ProjectCard {...item} />
