b1b6e9c03b0d9babaea9f20de0dd276a0d65d0a0
Ryan Gautier <ryangautier2@gmail.com>
2020-05-20 19:17:19 -0500

    responsive styling on home page

--- src/components/HomeContent/index.js
+++ src/components/HomeContent/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function HomeContent() {
   return (
     <div className="relative flex home-content items-center justify-center">
-      <div className="text-gray-100 text-5xl text-center home-text">
+      <div className="text-gray-100 text-4xl sm:text-5xl text-center home-text">
         My name is Ryan Gautier, and I am a Web Developer.
       </div>
     </div>
